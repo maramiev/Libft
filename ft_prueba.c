9d167c72219f2261f2a5839b9b60880af8edd8cd
maramiev <majocba20@gmail.com>
2023-12-12 14:54:27 +0100

    nvos cambios

--- ft_prueba.c
+++ ft_prueba.c
@@ -1,31 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_prueba.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maramiev <maramiev@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/27 13:46:43 by maramiev          #+#    #+#             */
-/*   Updated: 2023/12/12 14:52:40 by maramiev         ###   ########.fr       */
+/*   Created: 2023/12/12 14:46:10 by maramiev          #+#    #+#             */
+/*   Updated: 2023/12/12 14:52:55 by maramiev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-int	ft_isalpha(int c)
-
-{
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-	{
-		return (1);
-	}
-	return (0);
-}
-/*int	main(void)
-{
-	int	string1 = 'H';
-	
-	printf("El resultado aplicando mi funcion es : %d\n", ft_isalpha(string1));	 
-	printf("El resultado con la f original es : %d\n", isalpha(string1));
-	return(0);
-}*/
